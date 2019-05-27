79ae3f232a36e2717db55c6acfe7d740c580b2fa
Jian Liu <onanying@126.com>
2019-05-27 16:33:06 +0800

    处理文件没有扩展名的情况

--- src/UploadFile.php
+++ src/UploadFile.php
@@ -5,25 +5,41 @@ namespace Mix\Http\Message;
 use Mix\Helper\RandomStringHelper;
 
 /**
- * UploadFile类
+ * Class UploadFile
+ * @package Mix\Http\Message
  * @author liu,jian <coder.keda@gmail.com>
  */
 class UploadFile
 {
 
-    // 文件名
+    /**
+     * 文件名
+     * @var string
+     */
     public $name;
 
-    // MIME类型
+    /**
+     * MIME类型
+     * @var string
+     */
     public $type;
 
-    // 临时文件
+    /**
+     * 临时文件
+     * @var string
+     */
     public $tmpName;
 
-    // 错误码
+    /**
+     * 错误码
+     * @var int
+     */
     public $error;
 
-    // 文件尺寸
+    /**
+     * 文件尺寸
+     * @var int
+     */
     public $size;
 
     /**
@@ -37,7 +53,10 @@ class UploadFile
         return is_null($file) ? $file : new static($file);
     }
 
-    // 构造
+    /**
+     * UploadFile constructor.
+     * @param $file
+     */
     public function __construct($file)
     {
         $this->name    = $file['name'];
@@ -47,7 +66,11 @@ class UploadFile
         $this->size    = $file['size'];
     }
 
-    // 文件另存为
+    /**
+     * 文件另存为
+     * @param $filename
+     * @return bool
+     */
     public function saveAs($filename)
     {
         $dir = dirname($filename);
@@ -58,22 +81,33 @@ class UploadFile
         return $bytes ? true : false;
     }
 
-    // 获取基础名称
+    /**
+     * 获取基础名称
+     * @return mixed
+     */
     public function getBaseName()
     {
         return pathinfo($this->name)['filename'];
     }
 
-    // 获取扩展名
+    /**
+     * 获取扩展名
+     * @return string
+     */
     public function getExtension()
     {
-        return pathinfo($this->name)['extension'];
+        return pathinfo($this->name)['extension'] ?? '';
     }
 
-    // 获取随机文件名
+    /**
+     * 获取随机文件名
+     * @return string
+     */
     public function getRandomFileName()
     {
-        return md5(RandomStringHelper::randomAlphanumeric(32)) . '.' . $this->getExtension();
+        $extension = $this->getExtension();
+        $extension = $extension ? ".{$extension}" : '';
+        return md5(RandomStringHelper::randomAlphanumeric(32)) . $extension;
     }
 
 }
